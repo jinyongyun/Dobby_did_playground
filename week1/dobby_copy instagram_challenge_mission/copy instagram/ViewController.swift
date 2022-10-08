222ffa0e9e460db1f09b49423c7713142d407e7b
jinyongyun <102133961+jinyongyun@users.noreply.github.com>
2022-10-08 12:06:26 +0900

    Add files via upload

---
+++ week1/dobby_copy instagram_challenge_mission/copy instagram/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  copy instagram
+//
+//  Created by mac on 2022/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var LoginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        LoginButton.layer.cornerRadius = 10
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func LoginButtonact(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ week1/dobby_copy instagram_challenge_mission/copy instagram/secondloginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  secondloginViewController.swift
+//  copy instagram
+//
+//  Created by mac on 2022/09/20.
+//
+
+import UIKit
+
+class secondloginViewController: UIViewController {
+
+    @IBOutlet weak var secondLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        secondLoginButton.layer.cornerRadius = 10
+    }
+
+    
+}
+
